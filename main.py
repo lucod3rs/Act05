ea2e6d82ed85bbc6cbcf4a7dd2cc7c6de8b626ce
LuisC <ing.luiscuaical@gmail.com>
2026-05-19 20:41:53 -0500

    Importacion Librerias - archivos

---
+++ main.py
@@ -0,0 +1,12 @@
+# ==========================================
+# main.py
+# archivo principal del proyecto
+#======================================
+
+# Importar las librerías necesarias
+import tkinter as tk
+from tkinter import messagebox
+
+# Conectamos los módulos cofre y contrasena
+from contrasena import Contrasena, LongitudInvalidaError
+from cofre import Cofre

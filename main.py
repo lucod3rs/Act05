a096375678507264495e9f4c77ba0b7527c098c2
LuisC <ing.luiscuaical@gmail.com>
2026-05-19 20:44:20 -0500

    Clase JuegoCazador

--- main.py
+++ main.py
@@ -10,3 +10,17 @@ from tkinter import messagebox
 # Conectamos los módulos cofre y contrasena
 from contrasena import Contrasena, LongitudInvalidaError
 from cofre import Cofre
+
+
+# Clase principal del juego
+class JuegoCazador:
+    #Inicialización de la clase
+    def __init__(self):
+        self.puntaje = 0
+        self.nombre_jugador = ""
+
+       # Variables para la interfaz gráfica 
+        self.ventana = None
+        self.entrada_nombre = None
+        self.entrada_longitud = None
+        self.etiqueta_marcador = None

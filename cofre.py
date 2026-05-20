50322a0a38084814af245390dfb63e865816f6e9
LuisC <ing.luiscuaical@gmail.com>
2026-05-19 20:19:27 -0500

    metodo clase Cofre

--- cofre.py
+++ cofre.py
@@ -12,3 +12,15 @@ class Cofre:
     def __init__(self, nombre, puntos): #El método __init__ es el constructor de la clase Cofre. Recibe dos parámetros: nombre y puntos.
         self.nombre = nombre
         self.puntos = puntos
+
+   #Definimos un método de clase para generar un cofre aleatorio
+    @classmethod
+    def generar_cofre_aleatorio(cls):
+        #Elige al azar un cofre, cada uno con un nombre y una cantidad de puntos diferente
+        opciones = [
+            ("Comun", 10),      
+            ("Raro", 25),       
+            ("Legendario", 50)   
+        ]
+        nombre, puntos = random.choice(opciones)
+        return cls(nombre, puntos)
